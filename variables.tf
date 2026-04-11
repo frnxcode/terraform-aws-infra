44ece814b792b7875b7e2ccc0c18c8768c0bc715
frnxcode <olukayode-gomes@hotmail.com>
2026-04-11 16:16:44 +0100

    Add variables and provision EC2 instance

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  default     = "HelloWorld"
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  default     = "us-west-2"
+}
